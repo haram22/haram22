58d5e65aee4666ac949b68c963c7b0179aa61a57
haram22 <kimhr9889@handong.ac.kr>
2025-02-23 19:43:45 +0900

    chore: edit markdown

--- index.js
+++ index.js
@@ -43,8 +43,7 @@ let text = `### 🙈 Experiences
 </a>
 <br><br>
 
-<a href="https://hits.seeyoufarm.com"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fharam22%2F&count_bg=%23FD98E2&title_bg=%23555555&icon=&icon_color=%23DFC0C0&title=hits&edge_flat=false"/></a>
-
+### 🩵 Latest Blog Posts
 `;
 
 // rss-parser 생성
